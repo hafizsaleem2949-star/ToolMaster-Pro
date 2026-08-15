d2b6e9a511bba1f2253df87657ed29f67156a576
hafizsaleem2949-star <hafizsaleem2949@gmail.com>
2026-08-15 18:54:06 +0500

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,1 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://bqcjvrffhueqkleepevw.supabase.co'
+const supabasePublishableKey = 'sb_publishable_VUwOj9pZhFcMdhwKVsSJpw_aeAjnsfP'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabasePublishableKey
+)
